2d3732933f96ba033de6b906c607c78331fc7a6f
jigom11791 <jigom11791@gmail.com>
2022-01-26 22:13:55 -0800

    added gh-pages

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,27 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
+import Fade from 'react-reveal';
 
 function Section({title, description, backgroundImg, leftBtnText, rightBtnText}) {
     // console.log(props);
   return (
     <Wrap bgImage = { backgroundImg }>
-      <ItemText>
-          <h1>{ title }</h1>
-          <p>{ description }</p>
-      </ItemText>
+      <Fade bottom>
+        <ItemText>
+            <h1>{ title }</h1>
+            <p>{ description }</p>
+        </ItemText>
+      </Fade>
       <Buttons>
-        <ButtonGroup>
-            <LeftButton>
-                { leftBtnText }
-            </LeftButton>
-            {/* if right button exists then show right button*/}
-            { rightBtnText &&
-                <RightButton>
-                    { rightBtnText }
-                </RightButton>
-            }
-            
-        </ButtonGroup>
+        <Fade bottom>
+            <ButtonGroup>
+                <LeftButton>
+                    { leftBtnText }
+                </LeftButton>
+                {/* if right button exists then show right button*/}
+                { rightBtnText &&
+                    <RightButton>
+                        { rightBtnText }
+                    </RightButton>
+                }
+                
+            </ButtonGroup>
+        </Fade>
         <DownArrow src="/images/down-arrow.svg"></DownArrow>
       </Buttons>
     </Wrap>
@@ -32,6 +37,7 @@ function Section({title, description, backgroundImg, leftBtnText, rightBtnText})
 export default Section;
 
 const Wrap = styled.div`
+    z-index: 10;
     width: 100vw;
     height: 100vh;
     background-size: cover;
@@ -48,6 +54,7 @@ const Wrap = styled.div`
 const ItemText = styled.div`
     padding-top: 15vh;
     text-align: center;
+    z-index: 10;
 `
 
 const ButtonGroup = styled.div`
